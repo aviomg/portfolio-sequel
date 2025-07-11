b0f01514371a837daeff0b2272ea41caac88e77f
aviomg <avikumar@ad.unc.edu>
2025-07-11 00:15:40 -0400

    got the database connected poems thing up and running but still need minor tweaks

--- web/lib/notion.ts
+++ web/lib/notion.ts
@@ -1,4 +1,5 @@
 import {Client} from '@notionhq/client'
+import { Poem } from "../pages/blog";
 
 const notion = new Client({auth:process.env.NOTION_TOKEN});
 
@@ -22,6 +23,31 @@ export type CrochetImage = {
     caption: string | "",
     subcaption: string | "",
 }
+
+export type Block = {
+    id:string,
+    parent_id: string,
+    type: string,
+    content:string
+}
+
+export type PoemNotion = {
+    title: string;
+    date: string;
+    subtitle:string | null;
+    content: string[];
+    isarchive:boolean;
+  };
+
+/*export type Poem = {
+   title: string,
+   subtitle:string,
+   date: string,
+   content: string,
+   archive:string
+ }*/
+
+
 export async function getCourses(){
     const databaseId = process.env.NOTION_DATABASE_ID!;
     const response = await notion.databases.query({ 
@@ -57,6 +83,91 @@ export async function getCrochetPosts() {
     }));
   }
 
+  export async function getPoems(){
+  
+    const response = await notion.databases.query({
+        database_id:process.env.NOTION_POEM_DB_ID!,
+        sorts: [{ property: "order", direction: "ascending" }]
+    })
+
+    const poems:PoemNotion[] = [];
+    let cursor: string | undefined = undefined;
+
+
+    for(const page of response.results){
+        let isarchive:boolean = false;
+        const sel = page.properties.archive.select?.name || "";
+        if(sel=="Yes"){
+            isarchive = true;
+        }
+        const blocks = [];
+        
+        do{
+            const res = await notion.blocks.children.list({
+                block_id:page.id,
+                start_cursor: cursor,
+            });
+            blocks.push(...res.results);
+        } while(cursor);
+    
+
+    let title = "";
+    let date = "";
+    let subtitle=null;
+    const content:string[] = [];
+    let ind:number = 0;
+    let counter:number=1;
+
+    for(const block of blocks){
+       if(block.type==="paragraph"){
+        const bold = block.paragraph.rich_text[0]?.annotations.bold;
+        const italic = block.paragraph.rich_text[0]?.annotations.italic;
+       // const bold = annotations.bold;
+       if(bold){
+        const p = block.paragraph.rich_text[0]?.plain_text || '';
+        const headers = p.split("\n");
+        title = headers[1];
+        date = headers[0];
+        continue;}
+        if(italic){
+            const p = block.paragraph.rich_text[0]?.plain_text || '';
+            subtitle = p;
+            continue;
+        }
+       }
+       if(block.type==="paragraph"){
+        const p = block.paragraph.rich_text[0]?.plain_text || '';
+        content.push(p);
+       }
+       else if(block.type==="numbered_list_item"){
+        const p = block.numbered_list_item.rich_text[0]?.plain_text || '';
+        content.push(counter.toString() + ". " + p);
+        counter+=1;
+       }
+
+       else{content.push(block.type + " and ind is " + ind.toString())}
+       
+       /* else if(block.type === "paragraph"){
+            const paragraph = block.paragraph.rich_text[0]?.plain_text || '';
+            content.push(paragraph);
+        }
+        else{
+            const type  = block.type;
+            content.push(type + " and index is " + ind.toString());
+        }*/
+        ind+=1;
+    }
+    if(title && date && content.length){
+        poems.push({title,date,subtitle, content, isarchive});
+    }
+    }
+    return poems;
+ 
+
+
+  }
+  
+
 
 export async function getPostImages(postId:string){
     const response = await notion.databases.query({
@@ -80,6 +191,35 @@ export async function getPostImages(postId:string){
     }));
     
 }
+
+
+//we have a specific page in mind, which we know by the page ids listed in database
+
+export async function getPagesContent(pageId:string){
+    const blocks = [];
+    const response = await notion.blocks.children.list({
+          block_id: pageId,
+          page_size: 50,
+        });
+    
+        return response.results.map((block:any)=>{
+            const btype = block.type;
+            
+            if(btype=="paragraph"){
+                return block.paragraph?.rich_text[0]?.plain_text || '';
+
+            }
+            else if(btype=="heading_2"){
+                return block.heading_2?.rich_text[0]?.plain_text || '';
+
+            }
+        
+});// "hasmore": "hasnomore";
+
+    //return blocks[0].type;
+
+}
+
 
   
   
